8adb437a02431000335824aecacaba1bfd98618d
Susumu Yamazaki <zacky1972@gmail.com>
2022-09-05 23:09:54 +0900

    install benchee

--- mix.exs
+++ mix.exs
@@ -26,6 +26,7 @@ defmodule SampleNxOpenBlas.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:elixir_make, "~> 0.6", runtime: false},
+      {:benchee, "~> 1.1", only: :dev},
       {:nx, "~> 0.3"}
     ]
   end
