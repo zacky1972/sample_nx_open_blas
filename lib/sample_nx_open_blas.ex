2d9a7ef194694855e2879f50842d3202a1c8f270
Susumu Yamazaki <zacky1972@gmail.com>
2022-09-05 22:28:39 +0900

    implement temporally

--- lib/sample_nx_open_blas.ex
+++ lib/sample_nx_open_blas.ex
@@ -18,5 +18,47 @@ defmodule SampleNxOpenBlas do
     end
   end
 
-  def scopy_nif(_t), do: :erlang.nif_error(:not_loaded)
+
+  @doc """
+  Copys 32bit float.
+  ## Examples
+
+      iex> SampleNxOpenBlas.copy_f32(0)
+      #Nx.Tensor<
+        f32[1]
+        [0.0]
+      >
+
+      iex> SampleNxOpenBlas.copy_f32(Nx.tensor([0, 1, 2, 3]))
+      #Nx.Tensor<
+        f32[4]
+        [0.0, 1.0, 2.0, 3.0]
+      >
+
+  """
+  def copy_f32(t), do: copy(t, {:f, 32})
+
+  @doc false
+  def copy(t, type) when is_struct(t, Nx.Tensor) do
+    copy_sub(Nx.as_type(t, type), type)
+  end
+
+  @doc false
+  def copy(t, type) when is_number(t) do
+    copy(Nx.tensor([t]), type)
+  end
+
+  defp copy_sub(t, type) do
+    Nx.from_binary(copy_sub_sub(Nx.size(t), Nx.shape(t), Nx.to_binary(t), type), type)
+  end
+
+  defp copy_sub_sub(size, shape, binary, {:f, 32}) do
+    try do
+      scopy_nif(size, shape, binary)
+    rescue
+      e in ErlangError -> raise RuntimeError, message: List.to_string(e.original)
+    end
+  end
+
+  def scopy_nif(_size, _shape, _binary), do: :erlang.nif_error(:not_loaded)
 end
--- nif_src/libnif.c
+++ nif_src/libnif.c
@@ -3,13 +3,13 @@
 
 static ERL_NIF_TERM scopy_nif(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[])
 {
-    return enif_make_atom(env, "ok");
+    return argv[2];
 }
 
 
 static ErlNifFunc nif_funcs [] =
 {
-    {"scopy_nif", 1, scopy_nif}
+    {"scopy_nif", 3, scopy_nif}
 };
 
 ERL_NIF_INIT(Elixir.SampleNxOpenBlas, nif_funcs, NULL, NULL, NULL, NULL)
