c7eaf29dab786dd89ade4c6a2b8c333f921c935f
Susumu Yamazaki <zacky1972@gmail.com>
2022-09-05 19:13:56 +0900

    implement libnif temporally

--- lib/sample_nx_open_blas.ex
+++ lib/sample_nx_open_blas.ex
@@ -1,7 +1,24 @@
 defmodule SampleNxOpenBlas do
+  require Logger
+
   @moduledoc """
   Documentation for `SampleNxOpenBlas`.
   """
+
+  @on_load :load_nif
+
+  @doc false
+  def load_nif do
+    nif_file = ~c'#{Application.app_dir(:sample_nx_open_blas, "priv/libnif")}'
+
+    case :erlang.load_nif(nif_file, 0) do
+      :ok -> :ok
+      {:error, {:reload, _}} -> :ok
+      {:error, reason} -> Logger.error("Failed to load NIF: #{inspect(reason)}")
+    end
+  end
+
+  def copy_nif(_t), do: :erlang.nif_error(:not_loaded)
 
   @doc """
   Hello world.
--- nif_src/libnif.c
+++ nif_src/libnif.c
@@ -1,1 +1,14 @@
 #include <erl_nif.h>
+
+static ERL_NIF_TERM copy_nif(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[])
+{
+    return enif_make_atom(env, "ok");
+}
+
+
+static ErlNifFunc nif_funcs [] =
+{
+    {"copy_nif", 1, copy_nif}
+};
+
+ERL_NIF_INIT(Elixir.SampleNxOpenBlas, nif_funcs, NULL, NULL, NULL, NULL)
