96b979e7d8161d1b20fa26905b394c58cbc5f56a
Susumu Yamazaki <zacky1972@gmail.com>
2022-09-05 18:52:39 +0900

    initial commit

---
+++ lib/sample_nx_open_blas.ex
@@ -0,0 +1,18 @@
+defmodule SampleNxOpenBlas do
+  @moduledoc """
+  Documentation for `SampleNxOpenBlas`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SampleNxOpenBlas.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/sample_nx_open_blas_test.exs
@@ -0,0 +1,8 @@
+defmodule SampleNxOpenBlasTest do
+  use ExUnit.Case
+  doctest SampleNxOpenBlas
+
+  test "greets the world" do
+    assert SampleNxOpenBlas.hello() == :world
+  end
+end
